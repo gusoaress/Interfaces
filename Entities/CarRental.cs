28bab52c3b49d786c98f1df6c02f6b279ae851c7
Gustavo Soares <94994836+gusoaress@users.noreply.github.com>
2022-04-21 00:57:14 +0000

    Add services

---
+++ Entities/CarRental.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Rental.Entities
+{
+    class CarRental  
+    {
+        public DateTime Start {get; set;}
+        public DateTime Finish {get; set;}
+        public Vehicle Vehicle {get; set;}
+        public Invoice Invoice {get; set;}
+   
+
+        public CarRental (DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
+using Rental.Entities;
+using Rental.Services;
 
 class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Enter rental data"); 
+        Console.Write("Car model: ");
+        string model = Console.ReadLine(); 
+        Console.Write("Pick up date and time (dd/MM/yyyy HH:mm): ");
+        DateTime pickup = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture); 
+        Console.Write("Return date and time (dd/MM/yyyy HH:mm): ");
+        DateTime returndate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture); 
 
+        Console.Write("Enter price per hour: ");
+        double hour = double.Parse(Console.ReadLine()); 
+        Console.Write("Enter price per day: ");
+        double day = double.Parse(Console.ReadLine());
+        
+        
+        CarRental carRental = new CarRental (pickup, returndate, new Vehicle (model));
+        RentalService rentalService = new RentalService(hour, day);
+
+        
+        rentalService.ProcessInvoice(carRental);
+
+        
+        Console.WriteLine($"Invoice: {carRental.Invoice}"); 
+
+
+        
     }
-}        
+}         
